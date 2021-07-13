eff1c2eea685be786b856a28677504d57f8dca09
Anna Kowalewskaja <lao7777@ya.ru>
2021-07-13 22:25:51 +0300

    init: start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function toReadable (number) {
-  
+  let result = "";
+  let first =["one",	
+              "two",
+              "three",
+              "four",	
+              "five",	
+              "six",
+              "seven",	
+              "eight",
+              "nine",	
+              "ten",
+              "eleven",	
+              "twelve"
+           
+          ]
+
+  if ((0 < number) && (number < 13)) 
+  { result = first[number-1];}
+
+if((13<=number) && (number < 20)){
+  if ((number === 13) || (number === 15)){
+    (number === 13) ? (result = "thirteen") : (result = "fifteen");
+  } else{
+    let lastNum = number % 10;
+    result = first[lastNum - 1]+"teen";
+  }
+}
+
+return result;
+
+
+
 }
